421ac3c4d31f1ae2d52b09deb9a29c74bf45e1bb
unknown <jingbai7127@gmail.com>
2019-10-15 17:55:12 -0400

    122 easy (stock problem)

---
+++ 122. Best Time to Buy and Sell Stock II.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int maxProfit(vector<int>& prices) {
+	int profit = 0;
+    for(int i=1;i<prices.size();i++){
+    	if(prices[i]>prices[i-1]){
+    		profit += prices[i] - prices[i-1];
+    	}
+    }
+    return profit;
+}
+
+int main(){
+
+	return 0;
+}
