e4ee1c09babee895fc9841b4b8254803db995456
unknown <jingbai7127@gmail.com>
2019-10-15 21:03:05 -0400

    665 easy (math problem)

--- 665. Non-decreasing Array.cpp
+++ 665. Non-decreasing Array.cpp
@@ -7,31 +7,19 @@ bool checkPossibility(vector<int>& nums) {
     nums.insert(nums.begin(),INT_MIN);
     int cnt = 0;
     for(int i=1;i<nums.size()-1;i++){
-    	cout<<nums[i]<<", "<<nums[i+1]<<endl;
     	if(nums[i]>nums[i+1]){
-    		if(nums[i+1]>=nums[i-1]){
-    			if(cnt==0){
-	    			cnt++;
-	    		}
-	    		else
-	    			return false;
+    		if(nums[i+1]<nums[i-1]){
+	    		nums[i+1]=nums[i];
     		}
-    		else{
-    			if(cnt==0){
-	    			cnt++;
-	    			nums[i+1]=nums[i];
-	    		}
-	    		else
-	    			return false;
-    		}
+    		cnt++;
     	}
     }
-    return true;
+    return cnt<=1;
 }
 
 int main(){
-	int a[20] = {3,4,2,3};
-	vector<int> nums(a,a+4);
+	int a[20] = {4,2,1};
+	vector<int> nums(a,a+3);
 	cout<<checkPossibility(nums)<<endl;
 	return 0;
 }
