be6bcd0cd307a8d85f60d3c20b6ccf69366c056e
unknown <jingbai7127@gmail.com>
2019-11-02 12:23:58 -0400

    med (tree, queue)

---
+++ 513. Find Bottom Left Tree Value.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <string>
+#include <map>
+#include <queue>
+
+using namespace std;
+
+int findBottomLeftValue(TreeNode* root) {
+    int ans=0;
+    if(root == NULL)	return ans;
+    queue<TreeNode*> que;
+    que.push(root);
+    while(!que.empty()){
+        int size = que.size();
+        ans=que.front();
+        while(size){
+            TreeNode *temp = que.front();
+            que.pop();
+            if(temp->left!=NULL)
+                que.push(temp->left);
+            if(temp->right!=NULL)
+                que.push(temp->right);
+            size--;
+        }
+    }
+    return ans;
+}
+
+int main(){
+
+	return 0;
+}
